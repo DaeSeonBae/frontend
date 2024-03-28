8b560979aceb3a6e846ba7b3eab62b2565d186e2
dnwn1211 <dnwn3027@gmail.com>
2024-03-28 15:20:31 +0900

    main페이지 크기 조절 배치 완료
    main 페이지
    로그인 박스
    배너
    사이드
    푸터
    
    배치

--- DSB_front/src/component/main_body.js
+++ DSB_front/src/component/main_body.js
@@ -11,7 +11,7 @@ function MainBody() {
   };
 
   return (
-    <mainbody className='banner-box'>
+    <div>
       <div className="main_body">
         <div className="first">
           <div>
@@ -56,11 +56,18 @@ function MainBody() {
             </div>
           </div>
         </div>
-        <div className="three">
+        <div className="side">
           side
         </div>
-      </div>
-    </mainbody>
+        
+    </div>
+    <div className='footer-container'>
+    <div className='footer-box'>
+      <div className='team'>팀 : 범부</div>
+      <div className='rule'>이용약관, 개인정보처리방침, 청소년보호정책, 커뮤니티이용규칙, 공지사항, 문의하기, @대선배</div>
+    </div>
+  </div>
+    </div>
   );
 }
 
