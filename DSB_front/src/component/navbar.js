ee8db1a6b3eb4a1c97e8e76e78db15c719701b71
dnwn1211 <dnwn3027@gmail.com>
2024-03-31 01:17:17 +0900

    nav바 디자인 진행중
    nav바 디자인
    피그마 디자인으로 수정

--- DSB_front/src/component/navbar.js
+++ DSB_front/src/component/navbar.js
@@ -1,11 +1,24 @@
 import React from 'react';
-// import { FaStar, FaHeart } from 'react-icons/fa';
 import './navbar.css';
-import DarkModeIcon from '../images/darkmode.svg'; // 이미지 경로에 맞게 수정
+import DarkModeIcon from '../images/darkmode.svg';
 import LightModeIcon from '../images/lightmode.svg';
 import messageIcon from '../images/message.svg';
 import notificationIcon from '../images/notification.svg';
 import postlistIcon from '../images/post.svg';
+import timeIcon from '../images/time.svg';
+import sugangIcon from '../images/sugang.svg';
+import starIcon from '../images/star.svg';
+import aiIcon from '../images/AI.svg';
+import aplusIcon from '../images/Aplus.svg';
+import logoIcon from '../images/logo.png';
+
+import hotIcon from '../images/hot.svg';
+import flyIcon from '../images/fly.svg';
+import professorIcon from '../images/professor.svg';
+import gradeIcon from '../images/grade.svg';
+import studentIcon from '../images/student.svg';
+
+
 
 class Navbar extends React.Component {
   constructor(props) {
@@ -18,37 +31,63 @@ class Navbar extends React.Component {
   // 다크 모드 토글 함수
   toggleDarkMode = () => {
     this.setState({ isDarkMode: !this.state.isDarkMode });
-    // 여기서는 간단하게 body 요소에 클래스를 추가/제거하여 다크 모드를 구현합니다.
     document.body.classList.toggle('dark-mode');
   };
 
   render() {
     return (
       <nav className="nav">
+        <div className="nav_container">
         <div className="container1">
-          <div className="Logo">대선배</div>
+          <div className="Logo">
+            <img src={logoIcon} alt='aplusIcon' className="logoImage" /> 대선배
+          </div>
         </div>
-        <div className="container2">
-          <div className="nav-box">
-            <div className="nav-item">
-              <img src={postlistIcon} alt='postIcon'/>
+          <div className="container2">
+            <div className="nav-box">
+              <div class="nav-item">
+                <img src={postlistIcon} alt='postIcon'/>
+                <div>게시판</div>
+                <div class="hover-content">
+                  <p><img src={hotIcon} alt='hotIcon'/></p>
+                  <p><img src={flyIcon} alt='flyIcon'/></p>
+                  <p><img src={professorIcon} alt='professorIcon'/></p>
+                  <p><img src={gradeIcon} alt='gradeIcon'/></p>
+                  <p><img src={studentIcon} alt='studdentIcon'/></p>
+                </div>  
+              </div>
+              <div className="nav-item">
+                <img src={timeIcon} alt='timeIcon'/>
+                <div>시간표</div>
+              </div>
+              <div className="nav-item">
+                <img src={sugangIcon} alt='sugangIcon'/>
+                <div>수강신청</div>
+              </div>
+              <div className="nav-item">
+                <img src={starIcon} alt='starIcon'/>
+                <div>강의평가</div>
+              </div>
+              <div className="nav-item">
+                <img src={aiIcon} alt='aiIcon'/>
+                <div>AI</div>
+              </div>
+              <div className="nav-item">
+                <img src={aplusIcon} alt='aplusIcon'/>
+                <div>학점계산</div>
+              </div>
             </div>
-            <div className="nav-item">시간표</div>
-            <div className="nav-item">수강신청</div>
-            <div className="nav-item">강의평가</div>
-            <div className="nav-item">AI</div>
-            <div className="nav-item">학점계산</div>
           </div>
-        </div>
-        <div className="container3">
-          <div className="icon-box" onClick={this.toggleDarkMode}>
-            {this.state.isDarkMode ?  <img src={LightModeIcon} alt='Light Mode' />:<img src={DarkModeIcon} alt="Dark Mode" />}
-          </div>
-          <div className="icon-box" onClick={this.toggleFavorite}>
-            <img src={messageIcon} alt='messageIcon'/>
-          </div>
-          <div className="icon-box" onClick={this.toggleLike}>
-            <img src={notificationIcon} alt = 'notificationIcon'/>
+          <div className="container3">
+            <div className="icon-box" onClick={this.toggleDarkMode}>
+              {this.state.isDarkMode ?  <img src={LightModeIcon} alt='Light Mode' />:<img src={DarkModeIcon} alt="Dark Mode" />}
+            </div>
+            <div className="icon-box" onClick={this.toggleFavorite}>
+              <img src={messageIcon} alt='messageIcon'/>
+            </div>
+            <div className="icon-box" onClick={this.toggleLike}>
+              <img src={notificationIcon} alt = 'notificationIcon'/>
+            </div>
           </div>
         </div>
       </nav>
