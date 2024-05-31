17573f33d531dd80eb056c88c7d9deda6a004c4a
juchangyang <cksdid3357@naver.com>
2024-05-31 17:22:11 +0900

    관심사 추가 및 수정
    관심사 새로 설정하고, 수정함
    css는 card 크기 수정

--- DSB_front/src/component/interests.js
+++ DSB_front/src/component/interests.js
@@ -7,9 +7,7 @@ import logoIcon from '../images/DSB_logo.png';
 const Interests = () => {
 
   //각 버튼 클릭 상태를 관리할 state 설정
-  const [selectedFirstBtn, setSelectedFirstBtn] = useState(null);
-  const [selectedSecondtBtn, setSelectedSecondBtn] = useState(null);
-  const [selectedThirdBtn, setSelectedThirdBtn] = useState(null);
+  const [selectedInterests, setSelectedInterests] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false); //모달 오픈 여부
 
   //훅 사용
@@ -17,16 +15,17 @@ const Interests = () => {
   const skipNavigate = useNavigate();
 
   //버튼 클릭 시 버튼의 이름이 저장됨
-  const handleFirstBtnClick = (buttonName) => {
-    setSelectedFirstBtn(buttonName);
-  }
+  const handleInterestButtonClick = (interest) => {
+    // 이미 선택된 관심사인지 확인
+    const isAlreadySelected = selectedInterests.includes(interest);
 
-  const handleSecondBtnClick = (buttonName) => {
-    setSelectedSecondBtn(buttonName);
-  }
-
-  const handleThirdBtnClick = (buttonName) => {
-    setSelectedThirdBtn(buttonName);
+    if (isAlreadySelected) {
+      // 이미 선택된 관심사인 경우, 배열에서 제거
+      setSelectedInterests(selectedInterests.filter(item => item !== interest));
+    } else {
+      // 선택되지 않은 관심사인 경우, 배열에 추가
+      setSelectedInterests([...selectedInterests, interest]);
+    }
   }
 
   const handleNextBtnClick = () => {
@@ -63,28 +62,46 @@ const Interests = () => {
         <div class="select-group">
           <h4>여가활동 및 취미생활</h4>
           <div class="buttons">
-            <button type="button" name='song' className={selectedFirstBtn === 'song' ? 'selected' : ''} onClick={()=> handleFirstBtnClick('song')}>노래</button>
-            <button type="button" name='movie' className={selectedFirstBtn === 'movie' ? 'selected' : ''} onClick={()=> handleFirstBtnClick('movie')}>영화</button>
-            <button type="button" name='workOut' className={selectedFirstBtn === 'workOut' ? 'selected' : ''} onClick={()=> handleFirstBtnClick('workOut')}>운동</button>
-            <button type="button" name='game' className={selectedFirstBtn === 'game' ? 'selected' : ''} onClick={()=> handleFirstBtnClick('game')}>게임</button>
+            <button type="button" name="music" className={selectedInterests.includes('music') ? 'selected' : ''} onClick={() => handleInterestButtonClick('music')}>음악</button>
+            <button type="button" name='movie' className={selectedInterests.includes('movie') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('movie')}>영화</button>
+            <button type="button" name='sports' className={selectedInterests.includes('sports') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('sports')}>운동</button>
+            <button type="button" name='travel' className={selectedInterests.includes('travel') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('travel')}>여행</button>
+            <button type="button" name='art' className={selectedInterests.includes('art') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('art')}>미술</button>
+            <button type="button" name='drama' className={selectedInterests.includes('drama') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('drama')}>드라마</button>
+            <button type="button" name='game' className={selectedInterests.includes('game') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('game')}>게임</button>
           </div>
         </div>
         <div class="select-group">
-          <h4>노래</h4>
+          <h4>학업</h4>
           <div class="buttons">
-            <button type="button" name='k-pop' className={selectedSecondtBtn === 'k-pop' ? 'selected' : ''} onClick={()=> handleSecondBtnClick('k-pop')}>k-pop</button>
-            <button type="button" name='j-pop' className={selectedSecondtBtn === 'j-pop' ? 'selected' : ''} onClick={()=> handleSecondBtnClick('j-pop')}>j-pop</button>
-            <button type="button" name='pop' className={selectedSecondtBtn === 'pop' ? 'selected' : ''} onClick={()=> handleSecondBtnClick('pop')}>팝송</button>
-            <button type="button" name='ballade' className={selectedSecondtBtn === 'ballade' ? 'selected' : ''} onClick={()=> handleSecondBtnClick('ballade')}>발라드</button>
+            <button type="button" name='humanities' className={selectedInterests.includes('humanities') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('humanities')}>인문</button>
+            <button type="button" name='Engineering' className={selectedInterests.includes('Engineering') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('Engineering')}>공학</button>
+            <button type="button" name='lifeNhealth' className={selectedInterests.includes('lifeNhealth') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('lifeNhealth')}>생명보건</button>
+            <button type="button" name='ai' className={selectedInterests.includes('ai') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('ai')}>ai융합</button>
+            <button type="button" name='Art' className={selectedInterests.includes('Art') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('Art')}>예술</button>
+            <button type="button" name='kinesiology' className={selectedInterests.includes('kinesiology') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('kinesiology')}>체육</button>
+            <button type="button" name='management' className={selectedInterests.includes('management') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('management')}>경영</button>
           </div>
         </div>
         <div class="select-group">
           <h4>주요 관심 분야</h4>
           <div class="buttons">
-            <button type="button" name='opposite-sex' className={selectedThirdBtn === 'opposite-sex' ? 'selected' : ''} onClick={()=> handleThirdBtnClick('opposite-sex')}>이성</button>
-            <button type="button" name='friend' className={selectedThirdBtn === 'friend' ? 'selected' : ''} onClick={()=> handleThirdBtnClick('friend')}>친구</button>
-            <button type="button" name='freshman' className={selectedThirdBtn === 'freshman' ? 'selected' : ''} onClick={()=> handleThirdBtnClick('freshman')}>신입생</button>
-            <button type="button" name='returning-student' className={selectedThirdBtn === 'returning-student' ? 'selected' : ''} onClick={()=> handleThirdBtnClick('returning-student')}>복학생</button>
+            <button type="button" name='opposite-sex' className={selectedInterests.includes('opposite-sex') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('opposite-sex')}>이성</button>
+            <button type="button" name='friend' className={selectedInterests.includes('friend') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('friend')}>친구</button>
+            <button type="button" name='freshman' className={selectedInterests.includes('freshman') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('freshman')}>신입생</button>
+            <button type="button" name='returning-student' className={selectedInterests.includes('returning-student') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('returning-student')}>복학생</button>
+            <button type="button" name='graduateSchool' className={selectedInterests.includes('graduateSchool') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('graduateSchool')}>대학원</button>
+          </div>
+        </div>
+        <div class="select-group">
+          <h4>주요 관심 분야</h4>
+          <div class="buttons">
+            <button type="button" name='employment' className={selectedInterests.includes('employment') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('employment')}>취업</button>
+            <button type="button" name='startups' className={selectedInterests.includes('startups') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('startups')}>친구</button>
+            <button type="button" name='careerExploration' className={selectedInterests.includes('careerExploration') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('careerExploration')}>진로탐색</button>
+            <button type="button" name='socialActivities' className={selectedInterests.includes('socialActivities') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('socialActivities')}>사회활동</button>
+            <button type="button" name='service' className={selectedInterests.includes('service') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('service')}>봉사</button>
+            <button type="button" name='internship' className={selectedInterests.includes('internship') ? 'selected' : ''} onClick={()=> handleInterestButtonClick('internship')}>인턴십</button>
           </div>
         </div>
         <div class="moveBtn">
