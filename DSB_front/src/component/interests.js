f81cb3c4ff4919219e401494c57791b8c809d1a2
juchangyang <cksdid3357@naver.com>
2024-06-10 21:31:10 +0900

    공통 css 설정
    공통 css 설정

--- DSB_front/src/component/interests.js
+++ DSB_front/src/component/interests.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import '../component_style/interests.css';
+import '../component_style/common.css';
 import logoIcon from '../images/DSB_logo.png';
 
 const Interests = () => {
