01664797577183ab0594c82bb52c408c6f118a06
juchangyang <cksdid3357@naver.com>
2024-06-13 01:31:06 +0900

    푸터 수정
    각각 div 나눔, 문의하기 링크 추가, css 수정

--- DSB_front/src/component/footer.js
+++ DSB_front/src/component/footer.js
@@ -7,8 +7,10 @@ const Footer = () => {
   return (
     <div className='footer-container'>
       <div className='footer-box'>
-        <div className='team'>팀 : 범부</div>
-        <div className='rule'>이용약관, 개인정보처리방침, 청소년보호정책, 커뮤니티이용규칙, 공지사항, 문의하기, @대선배</div>
+        {/* <div className='team'>팀 : 범부</div> */}
+        <div className='announcement'>공지사항</div>
+        <a className='inquiry' href="mailto:cksdid3357@gmail.com">문의하기</a>
+        <div className='daeseonbae'>대선배</div>
       </div>
     </div>
   );
