3209f4ce208afbb9d437ecb32d2c267b760cbed4
juchangyang <cksdid3357@naver.com>
2024-08-14 13:41:28 +0900

    api주소 및 findinfo 내용 수정
    api주소 및 findinfo 내용 수정

--- DSB_front/src/component/findinfo.js
+++ DSB_front/src/component/findinfo.js
@@ -1,42 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../component_style/find_info.css';
 import logoIcon from '../images/DSB_logo.png';
 
 function Findinfo() {
+  const [email, setEmail] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+
+  const handleResetPassword = async () => {
+    if (!email || !newPassword) {
+      alert('이메일과 새로운 비밀번호를 입력하세요');
+      return;
+    }
+
+    try {
+      const response = await fetch('/user/reset-password', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email,
+          newPassword: newPassword
+        })
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        alert('비밀번호가 성공적으로 재설정되었습니다.');
+      } else {
+        alert('비밀번호 재설정에 실패했습니다. 다시 시도하세요.');
+      }
+    } catch (error) {
+      alert('비밀번호 재설정 중 오류가 발생했습니다.');
+    }
+  };
+
   return (
     <div>
-      <div class="container">
-        <div class="card">
+      <div className="container">
+        <div className="card">
           <h2>
-            <Link to='/' className='login_link'>
-              <img className='DSB_logo' src={logoIcon} alt = 'logoIcon'/>
-            </Link>
+            <Link to='/' className='login_link'><img className='DSB_logo' src={logoIcon} alt='logoIcon' /></Link>
           </h2>
-          <h2>아이디 찾기</h2>
+          <h2>비밀번호 재설정</h2>
           <form>
-            <div class="input-group">
-              <input type="text" id="username" name="username" placeholder='가입한 이메일을 입력하세요'/>
+            <div className="input-group">
+              <input 
+                type="email" 
+                id="email" 
+                name="email" 
+                placeholder='이메일을 입력하세요' 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
-            <div class="input-group">
-              <input type="password" id="password" name="password" placeholder='이름을 입력하세요'/>
+            <div className="input-group">
+              <input 
+                type="password" 
+                id="newPassword" 
+                name="newPassword" 
+                placeholder='새로운 비밀번호를 입력하세요' 
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+              />
             </div>
-              <button type="button">아이디 찾기</button>
-          </form>
-        </div>
-        <div class="card">
-          <h2>
-            <Link to='/' className='login_link'><img className='DSB_logo' src={logoIcon} alt = 'logoIcon'/></Link>
-            </h2>
-          <h2>비밀번호 찾기</h2>
-          <form>
-            <div class="input-group">
-              <input type="text" id="username" name="username" placeholder='아이디를 입력하세요'/>
-            </div>
-            <div class="input-group">
-              <input type="password" id="password" name="password" placeholder='인증번호를 입력하세요'/>
-            </div>
-              <button type="button">비밀번호 찾기</button>
+            <button type="button" onClick={handleResetPassword}>비밀번호 재설정</button>
           </form>
         </div>
       </div>
