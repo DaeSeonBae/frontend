3152a8f26c3a169a4f357a17cf4e07342a1bcd07
dnwn1211 <dnwn3027@gmail.com>
2024-08-14 15:16:36 +0900

    main
    main
    - api 서버주소 수정

--- DSB_front/src/component/login.js
+++ DSB_front/src/component/login.js
@@ -26,7 +26,7 @@ function Login() {
       formDataToSend.append('email', formData.email);
       formDataToSend.append('password', formData.password);
   
-      const response = await fetch('http://api.daeseonbae.com:8080/api/login', {
+      const response = await fetch('/api/login', {
         method: 'POST',
         body: formDataToSend,
         headers: {
